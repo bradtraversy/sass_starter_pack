b8bfee66dffb31f0eaf5ff735e84d7dd80f34f48
Brad Traversy <me@bradtraversy.com>
2017-08-22 08:05:16 -0400

    version 1.0.0

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const gulp        = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass        = require('gulp-sass');
+
+// Compile Sass & Inject Into Browser
+gulp.task('sass', function() {
+    return gulp.src(['src/scss/*.scss'])
+        .pipe(sass())
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
+});
+
+
+// Watch Sass & Serve
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        server: "./src"  
+    });
+
+    gulp.watch(['src/scss/*.scss'], ['sass']);
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+});
+
+// Default Task
+gulp.task('default', ['serve']);
