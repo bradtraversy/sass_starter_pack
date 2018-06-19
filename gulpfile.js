3ffc060a36ad860688577652c4dea7c6ec2bc6f4
Brandon Reid <breid@criticalmix.com>
2018-06-18 17:43:32 -0700

    Add autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
-const gulp        = require('gulp');
-const browserSync = require('browser-sync').create();
-const sass        = require('gulp-sass');
+const gulp         = require('gulp');
+const browserSync  = require('browser-sync').create();
+const sass         = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 // Compile Sass & Inject Into Browser
 gulp.task('sass', function() {
     return gulp.src(['src/scss/*.scss'])
         .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
         .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
 });
